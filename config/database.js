86b6216ef8fb22ac744616d7c98f3e2f4af1cfeb
Jaydeep Verma <verma.jaydeep@yahoo.com>
2022-05-09 21:30:59 +0530

    added db config

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 module.exports = {
     'secret':'nodeauthsecret',
-    'database': 'mongodb://localhost:27017/devdb',
+    // 'database': 'mongodb://localhost:27017/devdb',
+    'database': process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost:27017/devdb',
     'expiresIn': '12h', // 12 hrs
   };
   
