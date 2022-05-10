e40d0447a6527a8a6294371925363ca03fa1f7b8
Jaydeep Verma <verma.jaydeep@yahoo.com>
2022-05-10 10:04:43 +0530

    mongo end point

--- config/database.js
+++ config/database.js
@@ -1,9 +1,7 @@
-const dotenv = require('dotenv').config()
-
 module.exports = {
     'secret':'nodeauthsecret',
     // 'database': 'mongodb://localhost:27017/devdb',
-    'database': process.env.ENV != 'dev' ? process.env.MONGODB_URI : 'mongodb://localhost:27017/devdb',
+    'database': 'mongodb+srv://appdb:5ClYhwKI5YfAVmA5@ui-and-cms-db.iyfrd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true',
     'expiresIn': '12h', // 12 hrs
   };
   
