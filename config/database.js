cccfaf6b3c6ea29671ffe0bc33abf91f43a47f28
Jaydeep Verma <verma.jaydeep@yahoo.com>
2022-08-18 12:07:49 +0530

    commented db config

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     'secret':'nodeauthsecret',
-    'database': 'mongodb://localhost:27017/devdb',
-    // 'database': 'mongodb+srv://appdb:5ClYhwKI5YfAVmA5@ui-and-cms-db.iyfrd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true',
+    // 'database': 'mongodb://localhost:27017/devdb',
+    'database': 'mongodb+srv://appdb:5ClYhwKI5YfAVmA5@ui-and-cms-db.iyfrd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true',
     'expiresIn': '12h', // 12 hrs
   };
   
